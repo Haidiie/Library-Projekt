ec3739e0214abb15888e9fa9facb1d3cb43aba16
HeidarSelman <heidar.selman@hotmail.com>
2020-09-27 20:33:45 +0200

    sixthcommit

--- LibraryManagment/build/generated-sources/ap-source-output/entity/Book_.java
+++ LibraryManagment/build/generated-sources/ap-source-output/entity/Book_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-09-27T13:49:20")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-09-27T18:20:33")
 @StaticMetamodel(Book.class)
 public class Book_ { 
 
